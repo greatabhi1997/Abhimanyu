01e7c89eb71c2620dae36e1fc892116b4f228e68
Abhimanyu <champabhi1997@gmail.com>
2016-09-27 11:16:34 +0530

    Add files via upload

---
+++ list.py
@@ -0,0 +1,5 @@
+lst=['abcd',786,2.23,'john',70.21]
+print(lst) # print complete list
+print(lst[0]) # print first element
+print(lst[1:3]) # print element starting from 2nd till 3rd
+print(lst[2:1]) # print element starting from 3rd element
